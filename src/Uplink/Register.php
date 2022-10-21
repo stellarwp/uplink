88d681f744ac69fd49b03af23dac34103b03eb87
MlKilderkin <mlkilderkin@gmail.com>
2022-10-21 19:12:09 +0300

    Add license class to register method

--- src/Uplink/Register.php
+++ src/Uplink/Register.php
@@ -16,11 +16,12 @@ class Register {
 	 * @param string $path Resource path to bootstrap file.
 	 * @param string $class Resource class.
 	 * @param string $version Resource version.
+	 * @param string $license_class Resource license class
 	 *
 	 * @return Resources\Resource
 	 */
-	public static function plugin( $name, $slug, $path, $class, $version ) {
-		return Resources\Plugin::register( $name, $slug, $path, $class, $version );
+	public static function plugin( $name, $slug, $path, $class, $version, $license_class = null ) {
+		return Resources\Plugin::register( $name, $slug, $path, $class, $version, $license_class );
 	}
 
 	/**
@@ -33,10 +34,11 @@ class Register {
 	 * @param string $path Resource path to bootstrap file.
 	 * @param string $class Resource class.
 	 * @param string $version Resource version.
+	 * @param string $license_class Resource license class.
 	 *
 	 * @return Resources\Resource
 	 */
-	public static function service( $name, $slug, $path, $class, $version ) {
-		return Resources\Service::register( $name, $slug, $path, $class, $version );
+	public static function service( $name, $slug, $path, $class, $version, $license_class = null ) {
+		return Resources\Service::register( $name, $slug, $path, $class, $version, $license_class );
 	}
 }

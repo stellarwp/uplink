1e02a6098d77dc96ecbdddd3e40579ef5f5dc6b8
MlKilderkin <mlkilderkin@gmail.com>
2022-10-27 18:33:15 +0300

    LICENS-8 Stub basic license page

--- src/Uplink/Admin/Settings.php
+++ src/Uplink/Admin/Settings.php
@@ -2,30 +2,47 @@
 
 namespace StellarWP\Uplink\Admin;
 
-
 class Settings extends Page {
 
-    protected string $path = '/resources/views/settings.php';
+	protected string $path = '/resources/views/settings.php';
 
-    public function add_admin_pages() {
-        $this->maybe_register_settings_page();
-    }
+	public function add_admin_pages(): void {
+		$plugin = $this->get_plugin();
 
-    public function maybe_register_settings_page() {
-        $this->register_page(
-            [
-                'id'       => 'uplink-licensing',
-                'parent'   => 'options-general.php',
-                'path'     => 'uplink-licensing',
-                'title'    => esc_html__( 'Uplink Licensing', 'stellarwp-uplink' ),
-                'icon'     => $this->get_menu_icon(),
-                'position' => 6,
-            ]
-        );
-    }
+		$this->register_page(
+			[
+				'id'       => sanitize_title( $plugin->get_name() ),
+				'parent'   => 'options-general.php',
+				'path'     => sanitize_title( $plugin->get_name() ),
+				'title'    => esc_html__( sprintf( '%s Licensing', $plugin->get_name() ), 'stellarwp-uplink' ),
+				'icon'     => $this->get_menu_icon(),
+				'position' => 6,
+			]
+		);
+	}
 
-    public function render_page() {
-        echo $this->get_content( [] );
-    }
+	public function maybe_add_network_settings_page() {
+		$plugin = $this->get_plugin();
+
+		if ( ! is_plugin_active_for_network( $plugin->get_path() ) ) {
+			return;
+		}
+
+		$this->register_page(
+			[
+				'id'         => sanitize_title( $plugin->get_name() ),
+				'parent'     => 'settings.php',
+				'path'       => sanitize_title( $plugin->get_name() ),
+				'title'      => esc_html__( sprintf( '%s Licensing', $plugin->get_name() ), 'stellarwp-uplink' ),
+				'icon'       => $this->get_menu_icon(),
+				'capability' => 'manage_network_options',
+				'position'   => 6,
+			]
+		);
+	}
+
+	public function render_page(): void {
+		echo $this->get_content( [] );
+	}
 
 }

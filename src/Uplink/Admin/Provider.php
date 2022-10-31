9141f91d5a15d51e977be8cfaabc296756266617
MlKilderkin <mlkilderkin@gmail.com>
2022-10-31 19:41:19 +0200

    Rework expired notices

--- src/Uplink/Admin/Provider.php
+++ src/Uplink/Admin/Provider.php
@@ -10,11 +10,13 @@ class Provider extends \tad_DI52_ServiceProvider {
 	 */
 	public function register() {
 		$this->container->singleton( Plugins_Page::class, Plugins_Page::class );
+		$this->container->singleton( Notice::class, Notice::class );
 
 		$this->register_hooks();
 	}
 
 	public function register_hooks(): void {
 		add_action( 'admin_enqueue_scripts', $this->container->callback( Plugins_Page::class, 'display_plugin_messages' ) );
+		add_action( 'admin_notices', $this->container->callback( Notice::class, 'setup_notices' ) );
 	}
 }

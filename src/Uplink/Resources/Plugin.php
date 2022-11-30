cc4cb1e3485d60517c2d1893b510434161413da3
MlKilderkin <mlkilderkin@gmail.com>
2022-11-30 19:14:56 +0200

    LICENS-23 Test Plugin resource

--- src/Uplink/Resources/Plugin.php
+++ src/Uplink/Resources/Plugin.php
@@ -29,8 +29,6 @@ class Plugin extends Resource {
 	 */
 	public static $update_status_option_prefix = 'stellar_uplink_update_status_';
 
-	public $res = null;
-
 	/**
 	 * Check for plugin updates.
 	 *
@@ -54,7 +52,6 @@ class Plugin extends Resource {
 		$this->set_update_status( $status );
 
 		$results        = $this->validate_license();
-		$this->res = $results;
 		$status->update = $results->get_raw_response();
 
 		if ( null !== $status->update ) {

7525030d14fa0381bb720eb490e5d505ae9b67cf
Justin Frydman <justin.frydman@gmail.com>
2023-10-20 10:36:19 -0600

    Register hook with singleton instance

--- src/Uplink/Notice/Provider.php
+++ src/Uplink/Notice/Provider.php
@@ -10,14 +10,12 @@ final class Provider extends Abstract_Provider {
 	 * @inheritDoc
 	 */
 	public function register(): void {
-		$this->container->bind( Notice_Controller::class, Notice_Controller::class );
-		$this->container->bind( Notice_Handler::class, static function ( $c ): Notice_Handler {
+		$this->container->singleton( Notice_Controller::class, Notice_Controller::class );
+		$this->container->singleton( Notice_Handler::class, static function ( $c ): Notice_Handler {
 			return new Notice_Handler( $c->get( Notice_Controller::class ) );
 		} );
 
-		add_action( 'admin_notices', function (): void {
-			$this->container->get( Notice_Handler::class )->display();
-		}, 12, 0 );
+		add_action( 'admin_notices', [ $this->container->get( Notice_Handler::class ), 'display' ], 12, 0 );
 	}
 
 }

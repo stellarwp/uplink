0da95a8cb8e5d99fa7a6d39a8d79429763b4a7c7
Justin Frydman <justin.frydman@gmail.com>
2023-11-21 12:28:08 -0700

    Separate out auth url builder, add uplink_slug to uplink_callback query variables

--- src/Uplink/functions.php
+++ src/Uplink/functions.php
@@ -3,6 +3,7 @@
 namespace StellarWP\Uplink;
 
 use StellarWP\Uplink\API\V3\Auth\Token_Authorizer;
+use StellarWP\Uplink\Auth\Auth_Url_Builder;
 use StellarWP\Uplink\Auth\Token\Contracts\Token_Manager;
 use StellarWP\Uplink\Components\Admin\Authorize_Button_Controller;
 use Throwable;
@@ -66,5 +67,26 @@ function is_authorized( string $license, string $token, string $domain ): bool {
 		}
 
 		return false;
+	}
+}
+
+/**
+ * Build a brand's authorization URL, with the uplink_callback base64 query variable.
+ *
+ * @param  string  $slug  The Product slug to render the button for.
+ * @param  string  $domain  An optional domain associated with a license key to pass along.
+ *
+ * @return string
+ */
+function build_auth_url( string $slug, string $domain = '' ): string {
+	try {
+		return Config::get_container()->get( Auth_Url_Builder::class )
+		                              ->build( $slug, $domain );
+	} catch ( Throwable $e ) {
+		if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
+			error_log( "Error building auth URL: {$e->getMessage()} {$e->getFile()}:{$e->getLine()} {$e->getTraceAsString()}" );
+		}
+
+		return '';
 	}
 }
--- tests/wpunit/API/V3/AuthUrlTest.php
+++ tests/wpunit/API/V3/AuthUrlTest.php
@@ -5,6 +5,7 @@ namespace StellarWP\Uplink\Tests\API\V3;
 use StellarWP\Uplink\API\V3\Auth\Auth_Url;
 use StellarWP\Uplink\API\V3\Auth\Auth_Url_Cache_Decorator;
 use StellarWP\Uplink\API\V3\Contracts\Client_V3;
+use StellarWP\Uplink\Auth\Auth_Url_Builder;
 use StellarWP\Uplink\Tests\UplinkTestCase;
 
 final class AuthUrlTest extends UplinkTestCase {
@@ -32,6 +33,32 @@ final class AuthUrlTest extends UplinkTestCase {
 		$auth_url = $this->container->get( Auth_Url::class )->get( 'kadence-blocks-pro' );
 
 		$this->assertSame( 'https://www.kadencewp.com/account-auth/', $auth_url );
+	}
+
+	public function test_it_builds_an_auth_url(): void {
+		$clientMock = $this->makeEmpty( Client_V3::class, [
+			'get' => static function () {
+				return [
+					'response' => [
+						'code' => 200,
+					],
+					'body'   => json_decode( '{"success":true,"data":{"status":200,"auth_url":"https://www.kadencewp.com/account-auth/"}}', true ),
+				];
+			},
+		] );
+
+		$this->container->bind( Client_V3::class, $clientMock );
+
+		$auth_url = $this->container->get( Auth_Url_Builder::class )->build( 'kadence-blocks', 'test.com' );
+
+		parse_str( parse_url( $auth_url, PHP_URL_QUERY ), $query_vars );
+
+		$callback = base64_decode( urldecode( $query_vars['uplink_callback'] ), true );
+
+		$this->assertStringStartsWith(
+			'http://wordpress.test/wp-admin/index.php?uplink_domain=test.com&uplink_slug=kadence-blocks&_uplink_nonce=',
+			$callback
+		);
 	}
 
 	public function test_it_does_not_get_an_auth_url(): void {

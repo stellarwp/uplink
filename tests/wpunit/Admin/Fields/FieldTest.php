9409cd52c5691ec41355b609c187249e4cffa6a1
Brian Krane <redscar@gmail.com>
2024-07-11 15:23:38 -0400

    Switched to dataprovider.

--- tests/wpunit/Admin/Fields/FieldTest.php
+++ tests/wpunit/Admin/Fields/FieldTest.php
@@ -2,16 +2,17 @@
 
 namespace StellarWP\Uplink\Admin\Fields;
 
-use ArrayIterator;
 use InvalidArgumentException;
 use StellarWP\Uplink\Admin\License_Field;
 use StellarWP\Uplink\Config;
 use StellarWP\Uplink\Register;
+use StellarWP\Uplink\Resources\Collection;
 use StellarWP\Uplink\Tests\UplinkTestCase;
-use StellarWP\Uplink\Uplink;
-use StellarWP\Uplink\Resources as Uplink_Resources;
+use StellarWP\Uplink\Tests\TestUtils;
 
 class FieldTest extends UplinkTestCase {
+
+	use TestUtils;
 
 	/**
 	 * The root directory for plugin/services paths.
@@ -19,84 +20,6 @@ class FieldTest extends UplinkTestCase {
 	 * @var string
 	 */
 	private string $root;
-
-	protected function setUp(): void {
-		parent::setUp();
-		$this->collection = $this->container->get( Uplink_Resources\Collection::class );
-		$resources        = $this->get_resources();
-		foreach ( $resources as $resource ) {
-			call_user_func(
-				Register::class . '::' . $resource['type'],
-				$resource['slug'],
-				$resource['name'],
-				$resource['version'],
-				$resource['path'],
-				$resource['class']
-			);
-		}
-	}
-
-	/**
-	 * Resources.
-	 */
-	public function get_resources(): array {
-		$root =  dirname( __DIR__, 3 );;
-		return [
-			[
-				'slug'          => 'plugin-1',
-				'name'          => 'Plugin 1',
-				'path'          => $root . '/plugin.php',
-				'class'         => Uplink::class,
-				'license_class' => Uplink::class,
-				'version'       => '1.0.0',
-				'type'          => 'plugin',
-			],
-			[
-				'slug'          => 'plugin-2',
-				'name'          => 'Plugin 2',
-				'path'          => $root . '/plugin.php',
-				'class'         => Uplink::class,
-				'license_class' => Uplink::class,
-				'version'       => '2.0.0',
-				'type'          => 'plugin',
-			],
-			[
-				'slug'          => 'service-1',
-				'name'          => 'Service 1',
-				'path'          => $root . '/service1.php',
-				'class'         => Uplink::class,
-				'license_class' => Uplink::class,
-				'version'       => '3.0.0',
-				'type'          => 'service',
-			],
-			[
-				'slug'          => 'service-2',
-				'name'          => 'Service 2',
-				'path'          => $root . '/service2.php',
-				'class'         => Uplink::class,
-				'license_class' => Uplink::class,
-				'version'       => '4.0.0',
-				'type'          => 'service',
-			],
-		];
-	}
-
-	public function create_collection(){
-		$plugins   = array_slice( $this->get_resources(), 0, 2 );
-		$resources = [];
-
-		foreach ( $plugins as $slug => $plugin ) {
-			$resources[ $slug ] = new Uplink_Resources\Plugin(
-				$plugin['slug'],
-				$plugin['name'],
-				$plugin['version'],
-				$plugin['path'],
-				$plugin['class']
-			);
-		}
-
-		return new Uplink_Resources\Collection( new ArrayIterator( $resources ) );
-	}
 
 	/**
 	 * @test
@@ -108,174 +31,274 @@ class FieldTest extends UplinkTestCase {
 		new Field( $slug );
 	}
 
-	/**
-	 * @test
-	 */
-	public function it_should_get_fields_with_slug() {
-		$collection = $this->create_collection();
+	public function resourceProvider() {
+		$resources = $this->get_test_resources();
 
-		foreach ( $collection as $resource ) {
-			$slug        = $resource->get_slug();
-			$field       = new Field( $slug );
-			$license_key = 'license_key' . $slug;
-			$option_name = $resource->get_license_object()->get_key_option_name();
-
-			// Update the license key to a known value.
-			update_option( $option_name, $license_key );
-			$option_value = get_option($option_name);
-			$this->assertEquals($option_value,$license_key);
-
-			$field_name = 'field-' . $slug;
-			$field->set_field_name( $field_name );
-
-			// Add assertions to verify the field properties and methods
-			$this->assertEquals( $slug, $field->get_slug() );
-			$this->assertEquals( $resource->get_path(), $field->get_product() );
-			$this->assertEquals( $field_name, $field->get_field_name() );
-			$this->assertEquals( 'stellarwp_uplink_license_key_' . $slug, $field->get_field_id() );
-			$this->assertEquals( $license_key, $field->get_field_value(), 'Field value should be equal to the license key' );
-			$this->assertStringContainsString( 'A valid license key is required for support and updates', $field->get_key_status_html() );
-			$this->assertEquals( 'License key', $field->get_placeholder() );
-			$this->assertEquals( 'stellarwp-uplink-license-key-field', $field->get_classes() );
-
+		foreach ( $resources as $resource ) {
+			yield [ $resource ];
 		}
 	}
 
 	/**
 	 * @test
+	 * @dataProvider resourceProvider
 	 */
-	public function it_should_set_and_get_field_id() {
-		foreach ( $this->get_resources() as $resource ) {
-			$slug  = $resource['slug'];
-			$field = new Field( $slug );
+	public function it_should_get_fields_with_slug( $resource ) {
+		$collection = Config::get_container()->get( Collection::class );
 
-			$field_id = 'custom-field-id';
-			$field->set_field_id( $field_id );
+		Register::{$resource['type']}(
+			$resource['slug'],
+			$resource['name'],
+			$resource['version'],
+			$resource['path'],
+			$resource['class'],
+		);
 
-			$this->assertEquals( $field_id, $field->get_field_id() );
-		}
+		$current_resource = $collection->get( $resource['slug'] );
+		$slug             = $current_resource->get_slug();
+		$field            = new Field( $slug );
+		$license_key      = 'license_key' . $slug;
+		$option_name      = $current_resource->get_license_object()->get_key_option_name();
+
+		// Update the license key to a known value.
+		update_option( $option_name, $license_key );
+		$option_value = get_option( $option_name );
+		$this->assertEquals( $option_value, $license_key );
+
+		$field_name = 'field-' . $slug;
+		$field->set_field_name( $field_name );
+
+		// Add assertions to verify the field properties and methods
+		$this->assertEquals( $slug, $field->get_slug() );
+		$this->assertEquals( $current_resource->get_path(), $field->get_product() );
+		$this->assertEquals( $field_name, $field->get_field_name() );
+		$this->assertEquals( 'stellarwp_uplink_license_key_' . $slug, $field->get_field_id() );
+		$this->assertEquals( $license_key, $field->get_field_value(), 'Field value should be equal to the license key' );
+		$this->assertStringContainsString( 'A valid license key is required for support and updates', $field->get_key_status_html() );
+		$this->assertEquals( 'License key', $field->get_placeholder() );
+		$this->assertEquals( 'stellarwp-uplink-license-key-field', $field->get_classes() );
+
 	}
 
 	/**
 	 * @test
+	 * @dataProvider resourceProvider
 	 */
-	public function it_should_set_and_get_field_label() {
-		foreach ( $this->get_resources() as $resource ) {
-			$slug  = $resource['slug'];
-			$field = new Field( $slug );
+	public function it_should_set_and_get_field_id( $resource ) {
+		$collection = Config::get_container()->get( Collection::class );
+		Register::{$resource['type']}(
+			$resource['slug'],
+			$resource['name'],
+			$resource['version'],
+			$resource['path'],
+			$resource['class'],
+		);
 
-			$label = 'Custom Label';
-			$field->set_label( $label );
+		$current_resource = $collection->get( $resource['slug'] );
+		$slug             = $current_resource->get_slug();
+		$field            = new Field( $slug );
 
-			$this->assertEquals( $label, $field->get_label() );
-		}
+		$field_id = 'custom-field-id';
+		$field->set_field_id( $field_id );
+
+		$this->assertEquals( $field_id, $field->get_field_id() );
 	}
 
 	/**
 	 * @test
+	 * @dataProvider resourceProvider
 	 */
-	public function it_should_get_placeholder() {
-		foreach ( $this->get_resources() as $resource ) {
-			$slug  = $resource['slug'];
-			$field = new Field( $slug );
+	public function it_should_set_and_get_field_label( $resource ) {
+		$collection = Config::get_container()->get( Collection::class );
+		Register::{$resource['type']}(
+			$resource['slug'],
+			$resource['name'],
+			$resource['version'],
+			$resource['path'],
+			$resource['class'],
+		);
 
-			$this->assertEquals( 'License key', $field->get_placeholder() );
-		}
+		$current_resource = $collection->get( $resource['slug'] );
+		$slug             = $current_resource->get_slug();
+		$field            = new Field( $slug );
+
+		$label = 'Custom Label';
+		$field->set_label( $label );
+
+		$this->assertEquals( $label, $field->get_label() );
 	}
 
 	/**
 	 * @test
+	 * @dataProvider resourceProvider
 	 */
-	public function it_should_get_nonce_action_and_field() {
-		foreach ( $this->get_resources() as $resource ) {
-			$slug  = $resource['slug'];
-			$field = new Field( $slug );
+	public function it_should_get_placeholder( $resource ) {
+		$collection = Config::get_container()->get( Collection::class );
+		Register::{$resource['type']}(
+			$resource['slug'],
+			$resource['name'],
+			$resource['version'],
+			$resource['path'],
+			$resource['class'],
+		);
 
-			$nonce_action = Config::get_container()->get( License_Field::class )->get_group_name();
-			$nonce_field  = $field->get_nonce_field();
+		$current_resource = $collection->get( $resource['slug'] );
+		$slug             = $current_resource->get_slug();
+		$field            = new Field( $slug );
 
-			// Extract the nonce value from the nonce field
-			preg_match( '/value=["\']([^"\']+)["\']/', $nonce_field, $matches );
-			$nonce_value = $matches[1];
+		$this->assertEquals( 'License key', $field->get_placeholder() );
 
-			$this->assertNotEmpty( $nonce_action, 'Nonce action should not be empty.' );
-			$this->assertStringContainsString( 'stellarwp-uplink-license-key-nonce__' . $slug, $nonce_field, 'Nonce field should contain the correct action slug.' );
-
-			// Validate the nonce
-			$is_valid_nonce = wp_verify_nonce( $nonce_value, $nonce_action );
-			$this->assertContains( $is_valid_nonce, [ 1, 2 ], 'Nonce should be valid.' );
-		}
 	}
 
 	/**
 	 * @test
+	 * @dataProvider resourceProvider
 	 */
-	public function it_should_show_and_hide_label_and_heading() {
-		foreach ( $this->get_resources() as $resource ) {
-			$slug  = $resource['slug'];
-			$field = new Field( $slug );
+	public function it_should_get_nonce_action_and_field( $resource ) {
+		$collection = Config::get_container()->get( Collection::class );
+		Register::{$resource['type']}(
+			$resource['slug'],
+			$resource['name'],
+			$resource['version'],
+			$resource['path'],
+			$resource['class'],
+		);
 
-			$field->show_label( true );
-			$this->assertTrue( $field->should_show_label() );
+		$current_resource = $collection->get( $resource['slug'] );
+		$slug             = $current_resource->get_slug();
+		$field            = new Field( $slug );
 
-			$field->show_label( false );
-			$this->assertFalse( $field->should_show_label() );
+		$nonce_action = Config::get_container()->get( License_Field::class )->get_group_name();
+		$nonce_field  = $field->get_nonce_field();
 
-			$field->show_heading( true );
-			$this->assertTrue( $field->should_show_heading() );
+		// Extract the nonce value from the nonce field
+		preg_match( '/value=["\']([^"\']+)["\']/', $nonce_field, $matches );
+		$nonce_value = $matches[1];
 
-			$field->show_heading( false );
-			$this->assertFalse( $field->should_show_heading() );
-		}
+		$this->assertNotEmpty( $nonce_action, 'Nonce action should not be empty.' );
+		$this->assertStringContainsString( 'stellarwp-uplink-license-key-nonce__' . $slug, $nonce_field, 'Nonce field should contain the correct action slug.' );
+
+		// Validate the nonce
+		$is_valid_nonce = wp_verify_nonce( $nonce_value, $nonce_action );
+		$this->assertContains( $is_valid_nonce, [ 1, 2 ], 'Nonce should be valid.' );
+
 	}
 
 	/**
 	 * @test
+	 * @dataProvider resourceProvider
 	 */
-	public function it_should_render_correct_html() {
-		foreach ( $this->get_resources() as $resource ) {
-			$slug  = $resource['slug'];
-			$field = new Field( $slug );
+	public function it_should_show_and_hide_label_and_heading( $resource ) {
+		$collection = Config::get_container()->get( Collection::class );
+		Register::{$resource['type']}(
+			$resource['slug'],
+			$resource['name'],
+			$resource['version'],
+			$resource['path'],
+			$resource['class'],
+		);
 
-			$html = $field->render();
-			$this->assertStringContainsString( 'stellarwp-uplink-license-key-field', $html );
-			$this->assertStringContainsString( $slug, $html );
-		}
+		$current_resource = $collection->get( $resource['slug'] );
+		$slug             = $current_resource->get_slug();
+		$field            = new Field( $slug );
+
+		$field->show_label( true );
+		$this->assertTrue( $field->should_show_label() );
+
+		$field->show_label( false );
+		$this->assertFalse( $field->should_show_label() );
+
+		$field->show_heading( true );
+		$this->assertTrue( $field->should_show_heading() );
+
+		$field->show_heading( false );
+		$this->assertFalse( $field->should_show_heading() );
 	}
 
 	/**
 	 * @test
+	 * @dataProvider resourceProvider
 	 */
-	public function it_should_handle_empty_field_name() {
-		foreach ( $this->get_resources() as $resource ) {
-			$slug  = $resource['slug'];
-			$field = new Field( $slug );
+	public function it_should_render_correct_html($resource) {
+		$collection = Config::get_container()->get( Collection::class );
+		Register::{$resource['type']}(
+			$resource['slug'],
+			$resource['name'],
+			$resource['version'],
+			$resource['path'],
+			$resource['class'],
+		);
 
-			$this->assertEmpty( $field->get_field_name(), 'Field name should be empty by default' );
-		}
+		$current_resource = $collection->get( $resource['slug'] );
+		$slug             = $current_resource->get_slug();
+		$field            = new Field( $slug );
+
+		$html = $field->render();
+		$this->assertStringContainsString( 'stellarwp-uplink-license-key-field', $html );
+		$this->assertStringContainsString( $slug, $html );
 	}
 
 	/**
 	 * @test
+	 * @dataProvider resourceProvider
 	 */
-	public function it_should_handle_empty_field_id() {
-		foreach ( $this->get_resources() as $resource ) {
-			$slug  = $resource['slug'];
-			$field = new Field( $slug );
+	public function it_should_handle_empty_field_name( $resource ) {
+		$collection = Config::get_container()->get( Collection::class );
+		Register::{$resource['type']}(
+			$resource['slug'],
+			$resource['name'],
+			$resource['version'],
+			$resource['path'],
+			$resource['class'],
+		);
 
-			$this->assertNotEmpty( $field->get_field_id(), 'Field ID should not be empty even if not set' );
-		}
+		$current_resource = $collection->get( $resource['slug'] );
+		$slug             = $current_resource->get_slug();
+		$field            = new Field( $slug );
+
+		$this->assertEmpty( $field->get_field_name(), 'Field name should be empty by default' );
+
 	}
 
 	/**
 	 * @test
+	 * @dataProvider resourceProvider
 	 */
-	public function it_should_handle_empty_label() {
-		foreach ( $this->get_resources() as $resource ) {
-			$slug  = $resource['slug'];
-			$field = new Field( $slug );
+	public function it_should_handle_empty_field_id( $resource ) {
+		$collection = Config::get_container()->get( Collection::class );
+		Register::{$resource['type']}(
+			$resource['slug'],
+			$resource['name'],
+			$resource['version'],
+			$resource['path'],
+			$resource['class'],
+		);
 
-			$this->assertEmpty( $field->get_label(), 'Label should be empty by default' );
-		}
+		$current_resource = $collection->get( $resource['slug'] );
+		$slug             = $current_resource->get_slug();
+		$field            = new Field( $slug );
+
+		$this->assertNotEmpty( $field->get_field_id(), 'Field ID should not be empty even if not set' );
+	}
+
+	/**
+	 * @test
+	 * @dataProvider resourceProvider
+	 */
+	public function it_should_handle_empty_label( $resource ) {
+		$collection = Config::get_container()->get( Collection::class );
+		Register::{$resource['type']}(
+			$resource['slug'],
+			$resource['name'],
+			$resource['version'],
+			$resource['path'],
+			$resource['class'],
+		);
+
+		$current_resource = $collection->get( $resource['slug'] );
+		$slug             = $current_resource->get_slug();
+		$field            = new Field( $slug );
+
+		$this->assertEmpty( $field->get_label(), 'Label should be empty by default' );
 	}
 }

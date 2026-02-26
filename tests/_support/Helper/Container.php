0c95f22051368f1ed7f9f047112e802da7453c0f
Eric Defore <eric.defore@gmail.com>
2026-02-26 10:57:35 -0500

    SCON-94: Remove unnecessary override

--- tests/_support/Helper/Container.php
+++ tests/_support/Helper/Container.php
@@ -6,15 +6,4 @@ use StellarWP\ContainerContract\ContainerInterface;
 use lucatume\DI52\Container as DI52Container;
 
 class Container extends DI52Container implements ContainerInterface {
-
-	/**
-	 * Alias for get().
-	 *
-	 * @param string $id Identifier of the entry to look for.
-	 *
-	 * @return mixed
-	 */
-	public function make( $id ) {
-		return $this->get( $id );
-	}
 }

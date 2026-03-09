1c6048ba6643a3a5cc5c52a3105583672fcfc647
johnhooks <20690965+johnhooks@users.noreply.github.com>
2026-03-09 20:21:32 +0000

    Build frontend production assets

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-i18n'), 'version' => '3e096b56410cc9d98c1e');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-i18n'), 'version' => '71749d07495fd04dcbcb');

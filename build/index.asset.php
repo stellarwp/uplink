b7ed6c3cb32c10c7a363de5e0e3a525912233e0a
Estevão Costa <estevao90@gmail.com>
2026-03-09 14:10:56 -0300

    SCON-223: Debug CI

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-i18n'), 'version' => '097f2ef7b3ab1f2e2d35');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-i18n'), 'version' => '3e096b56410cc9d98c1e');

9da2c070470db1582b6010ce0b60c995c6df334f
John Hooks <bitmachina@outlook.com>
2026-03-17 09:21:12 -0700

    fix: add async UplinkError wrap (#198)
    Co-authored-by: johnhooks <20690965+johnhooks@users.noreply.github.com>

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '680682b0bb726999c3fb');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'a826fd859329e8d9d017');

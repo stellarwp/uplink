1579664cc7caf2a14bb45eb44b69e8d26ee05938
estevao90 <18039589+estevao90@users.noreply.github.com>
2026-03-17 18:04:27 +0000

    Build frontend assets

--- build-dev/index.asset.php
+++ build-dev/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '1e236e9042a6f7d717a8');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'd21dae8db6853846817b');
